c023b38943990f226ce989773bb5533dd2a0877d
lhuddlesto <lhuddlesto@gannett.com>
2019-01-25 10:47:25 -0500

    Factorialize A Number

---
+++ basic/alg3.py
@@ -0,0 +1,11 @@
+# Return the factorial of the provided integer.
+# If the integer is represented with the letter n, a factorial is the product of all positive integers less than or equal to n.
+# Factorials are often represented with the shorthand notation n!
+# For example: 5! = 1 * 2 * 3 * 4 * 5 = 120
+# Only integers greater than or equal to zero will be supplied to the function.
+import math
+
+def factorialize(num):
+    return math.factorial(num)
+
+print(factorialize(5))
